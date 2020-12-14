cd2ad6a872391ce19bb13830f1bbfa1b452a793a
pedro freitas <pedro.pauloy@outlook.com>
2020-12-14 13:45:59 -0300

    atividade 11-12

--- 11-12/atv-4.js
+++ 11-12/atv-4.js
@@ -1,10 +1,58 @@
-const timeout = (duration) => {
+//Exemplo 1
+function function_name(val) {
+  return val
+}
+
+//Exemplo 2
+function fun_1(...params) {
+  console.log(params.length)
+}
+
+//Exemplo 3
+var func = function(x,y){ return x*y }
+
+function product() {
+  var result
+  result = func(10, 20)
+  console.log("The product: " + result)
+}
+
+//Exemplo 4
+function fazRequisicao() {
   return new Promise((resolve, reject) => {
-    setTimeout(resolve, duration)
+      setTimeout(() => {
+          resolve("Promise Resolvida")
+      }, 5000)
   })
 }
 
-timeout(3000)
-  .then(function() { // executa o bloco após 3 segundos
-    console.log('passou 3 segundos')
-  })
+//Exemplo 5
+function buscarCep(param) {
+  let cep
+  fetch(`https://viacep.com.br/ws/${param}/json/`)
+      .then(response => response.json())
+      .then(data => {
+          cep = data.cep
+          console.log("CEP encontrado: " + cep)
+      })
+      .catch(console.error)
+  return cep
+}
+
+//Prática
+fetch = require("node-fetch")
+let pokemonBuscado
+pokemonBuscado = buscarPokemon("pikachu")
+console.log("Pokemon encontrado: " + pokemonBuscado)
+
+function buscarPokemon(param) {
+  let pokemon
+  fetch(`https://pokeapi.co/pokemon/${param}`)
+      .then(response => response.json())
+      .then(data => {
+        pokemon = data.pokemon
+          console.log("Pokemon encontrado: " + pokemon)
+      })
+      .catch(console.error)
+  return pokemon
+}
