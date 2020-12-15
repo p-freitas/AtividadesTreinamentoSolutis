569479531c0898710d8c5dd084a7fbf9125a6d5f
pedro freitas <pedro.pauloy@outlook.com>
2020-12-15 16:34:32 -0300

    adicionando nova tela de produtos

--- CRUD/server.js
+++ CRUD/server.js
@@ -46,6 +46,25 @@ app.post('/show', (req, res)=>{
       })
 });
 
+app.get('/showProduto', (req, res) => {
+  db.collection('produto').find().toArray((err, results) => {
+      if (err) return console.log(err)
+      console.log(results);
+      res.render('showProduto.ejs', { data: results })
+
+  })
+})
+ 
+app.post('/showProduto', (req, res)=>{
+    //criar a coleção “data”, que irá armazenar nossos dados
+    db.collection('produto').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+     
+        console.log('Salvo no Banco de Dados')
+        res.redirect('/showProduto')
+      })
+});
+
 app.route('/edit/:id')
 .get((req, res) => {
   var id = req.params.id
@@ -72,6 +91,38 @@ app.route('/edit/:id')
   })
 })
 
+app.route('/editProduto/:id')
+.get((req, res) => {
+  var id = req.params.id
+ 
+  db.collection('produto').find(ObjectId(id)).toArray((err, result) => {
+    if (err) return res.send(err)
+    res.render('editProduto.ejs', { data: result })
+  })
+})
+.post((req, res) => {
+  var id = req.params.id
+  var nome = req.body.nome
+  var preco = req.body.preco
+  var quantidade = req.body.quantidade
+  var data = req.body.data
+  var cor = req.body.cor
+ 
+  db.collection('produto').updateOne({_id: ObjectId(id)}, {
+    $set: {
+      nome: nome,
+      preco: preco,
+      quantidade: quantidade,
+      data: data,
+      cor: cor,
+    }
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.redirect('/showProduto')
+    console.log('Atualizado no Banco de Dados')
+  })
+})
+
 app.route('/delete/:id')
 .get((req, res) => {
   var id = req.params.id
@@ -80,5 +131,16 @@ app.route('/delete/:id')
     if (err) return res.send(500, err)
     console.log('Deletado do Banco de Dados!')
     res.redirect('/show')
+  })
+})
+
+app.route('/deleteProduto/:id')
+.get((req, res) => {
+  var id = req.params.id
+ 
+  db.collection('produto').deleteOne({_id: ObjectId(id)}, (err, result) => {
+    if (err) return res.send(500, err)
+    console.log('Deletado do Banco de Dados!')
+    res.redirect('/showProduto')
   })
 })
